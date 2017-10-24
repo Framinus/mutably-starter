fb45fb562d437d235ecacbb1b0e3ccd4c6d9f92a
James McCormack <thejamesmccormack@gmail.com>
2017-10-24 13:48:19 -0700

    createBook is working!

--- public/script.js
+++ public/script.js
@@ -1,7 +1,36 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  // code in here
+  const createBook = (event) => {
+    event.preventDefault();
+    const title = document.querySelector('.modal-title').value;
+    const author = document.querySelector('.modal-author').value;
+    const imageUrl = document.querySelector('.modal-image-url').value;
+    const releaseDate = document.querySelector('.modal-release-date').value;
+    fetch('https://mutably.herokuapp.com/books', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ title, author, imageUrl, releaseDate }),
+    })
+      .then(response => response.json())
+      .then((newbook) => {
+        console.log(newbook);
+      })
+      .catch(console.error);
+  };
+
+$('.create-button').on('click', createBook);
+
+
+
+
+
+
+
+
 
 });
